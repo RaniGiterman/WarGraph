7aed38943227ca121a22ed71f6fd1e0ac101850a
rani gi <ranigi@pop-os.localdomain>
2022-12-07 17:35:18 +0200

    latest

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 const appWidth = 800;
 const appHeight = 800;
 const target_amount = 8;
+const func_width = 3;
 const targetArr = [];
 
 const app = new PIXI.Application({
@@ -35,24 +36,30 @@ for (let i = 0; i < target_amount; i++) {
   targetArr.push({ x: target.x, y: target.y, r: ballSize, obj: target });
 }
 
-// draw y_axis
-var y_axis = new PIXI.Graphics();
-y_axis.lineStyle(1, 0x000000, 1);
-y_axis.position.x = appWidth / 2;
-y_axis.position.y = 0;
-y_axis.moveTo(5, 0);
-y_axis.lineTo(5, appHeight);
 
 // draw x_axis
 var x_axis = new PIXI.Graphics();
-x_axis.lineStyle(1, 0x000000, 1);
+x_axis.lineStyle(5, 0x000000, 1);
 x_axis.position.x = 0;
-x_axis.position.y = appHeight / 2;
-x_axis.moveTo(0, 5);
+x_axis.position.y = appHeight / 2 - 10;
+x_axis.moveTo(5, 5);
 x_axis.lineTo(appWidth, 5);
+x_axis.zIndex = 1;
+
+
+// draw x axis
+var y_axis = new PIXI.Graphics();
+y_axis.lineStyle(5, 0x000000, 1);
+y_axis.position.x = appWidth / 2;
+y_axis.position.y = 0;
+y_axis.moveTo(5, 5);
+y_axis.lineTo(5, appHeight);
+y_axis.zIndex = 1;
+
 
 app.stage.addChild(y_axis);
 app.stage.addChild(x_axis);
+
 
 // function fireDegree() {
 //   let val = document.getElementById("val").value;
@@ -70,29 +77,57 @@ app.stage.addChild(x_axis);
 
 //   app.stage.addChild(line);
 // }
+let lastEquation;
+function fire(eq, clearFunc) {
+  let equation = eq || document.getElementById("eq").value;
+  if (lastEquation && lastEquation != eq)
+    fire(lastEquation, true);
+  // for (let x = -appWidth / 2; x < appWidth / 2; x += 1) {
+  //   let y;
+  //   try {
+  //     // y = eval(equation.replace("x", `*(${x})`));
+  //     let s = interpret(equation, x);
+  //     y = eval(s);
+  //   } catch (error) {
+  //     continue;
+  //   }
 
-function fire() {
-  for (let x = -appWidth / 2; x < appWidth / 2; x += 1) {
-    let equation = document.getElementById("eq").value;
+  //   checkHitTarget(x + appWidth / 2, y * -1 + appHeight / 2);
+  //   draw(x, y);
+  // }
+  let x = -appWidth / 2;
+  let interval = setInterval(() => {
+    if (x >= appWidth / 2) return clearInterval(interval);
     let y;
     try {
       // y = eval(equation.replace("x", `*(${x})`));
       let s = interpret(equation, x);
       y = eval(s);
+      while ((y * -1 + appHeight / 2 > appHeight || y * -1 + appHeight / 2 < -appHeight) && x < appWidth / 2) { // want to skip drawing dots that are outside of screen
+        x++;
+        s = interpret(equation, x);
+        y = eval(s);
+      }
     } catch (error) {
-      continue;
+      console.log(error);
+      return clearInterval(interval);
     }
 
     checkHitTarget(x + appWidth / 2, y * -1 + appHeight / 2);
-    draw(x, y);
-  }
+    draw(x, y, clearFunc);
+    x += 2;
+  }, 4);
+
+  lastEquation = equation;
+  // to clean the screen off the last function, run the exact same process but color the dots white!!!
 }
 
 function interpret(equation, x) {
   let newEquation = "";
+  let power_of = null;
   for (let i = 0; i < equation.length; i++) {
     if (equation[i] == "x") {
-      if (!isNaN(equation[i - 1])) {
+      if (!isNaN(equation[i - 1]) && equation[i - 1] != " ") {
         // number before
         newEquation += `*(${x})`;
       } else {
@@ -103,6 +138,8 @@ function interpret(equation, x) {
     }
     if (equation[i] == "^") {
       newEquation += "**";
+      power_of = equation[i + 1];
+      // meaning next index gotta be the power of
       continue;
     }
     newEquation += equation[i];
@@ -112,17 +149,29 @@ function interpret(equation, x) {
   newEquation = newEquation.replace("cos", "Math.cos");
   newEquation = newEquation.replace("tan", "Math.tan");
 
+  if (power_of) {
+    newEquation = newEquation.replace(`-(${x})**${power_of}`, `-((${x})**${power_of})`);
+  }
+  // ^ Unary operator used immediately before exponentiation expression. Parenthesis must be used to disambiguate operator precedence
+
   return newEquation;
 }
 
-function draw(x, y) {
-  const func_width = 3;
+function draw(x, y, clearFunc) {
   const func_dot = new PIXI.Graphics();
-  func_dot.beginFill(0x000000);
-  func_dot.drawCircle(0, 0, func_width);
-  func_dot.endFill();
   func_dot.x = x + appWidth / 2;
   func_dot.y = y * -1 + appHeight / 2;
+  if (clearFunc) {
+    // func_dot.beginFill(0xffffff);
+    // if (x == 0) return;
+    for (let i = 0; i < app.stage.children.length; i++) {
+      if (app.stage.children[i].x == func_dot.x && app.stage.children[i].y == func_dot.y) app.stage.children.splice(i, 1);
+    }
+  }
+  else
+    func_dot.beginFill(0x000000);
+  func_dot.drawCircle(0, 0, func_width);
+  func_dot.endFill();
   app.stage.addChild(func_dot);
 }
 
